e5404ae39659c1218f722e8d95bd2c78e72e21f1
Aeryia <x_blyster_x@hotmail.fr>
2017-02-16 09:51:21 +0100

    test

--- app/main/constants/config-const.js
+++ app/main/constants/config-const.js
@@ -5,7 +5,7 @@ angular.module('main')
   // gulp environment: injects environment vars
   ENV: {
     /*inject-env*/
-    'SERVER_URL': '',
+    'SERVER_URL': "http://maderaapp.herokuapp.com/",
     'SOME_OTHER_URL': ''
     /*endinject*/
   },
