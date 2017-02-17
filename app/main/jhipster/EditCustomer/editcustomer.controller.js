c88c4bf7a67d60d8157479a2b62f79a97bc617cd
Aeryia <x_blyster_x@hotmail.fr>
2017-02-17 10:30:49 +0100

    Added EditCustomer + Updated EditCustomer

---
+++ app/main/jhipster/EditCustomer/editcustomer.controller.js
@@ -0,0 +1,82 @@
+(function()
+{
+	'use strict';
+	angular
+		.module('main')
+		.controller('EditCustomerController', EditCustomerController);
+
+	/**
+	 * Function who inject module for Angular
+	 * @type {Array}
+	 */
+	EditCustomerController.$inject = ['$scope', '$filter', 'Principal', '$state', '$location', '$ionicNavBarDelegate', 'Config', '$ionicSideMenuDelegate', '$rootScope', 'Customer','ParseLinks','$ionicConfig'];
+
+	/**
+	 * The index page controller
+	 * @param {[type]} $scope                 Scope module
+	 * @param {[type]} $filter                Filter module
+	 * @param {[type]} Principal              Principal module
+	 * @param {[type]} $state                 State module
+	 * @param {[type]} $location              Location module
+	 * @param {[type]} $ionicNavBarDelegate   IonicNavBarDelegate module
+	 * @param {[type]} Config                 Config module
+	 * @param {[type]} $ionicSideMenuDelegate IonicSideMenuDelegate module
+	 * @param {[type]} $rootScope             RootScope module
+	 * @param {[type]} PostSession            PostSession service
+	 * @param {[type]} GetTour                GetTour service
+	 * @param {[type]} GetTours               GetTours service
+	 * @param {[type]} $ionicLoading          IonicLoading module
+	 */
+	function EditCustomerController($scope, $filter, Principal, $state, $location, $ionicNavBarDelegate, Config, $ionicSideMenuDelegate, $rootScope, Customer, ParseLinks, $ionicConfig)
+	{
+		///////////////
+		// VARIABLES //
+		///////////////
+
+
+
+        //A appeler dans les autre state pour réactiver la transition
+        $ionicConfig.views.transition('none');
+
+		//////////////////////////
+		// Controller variables //
+		//////////////////////////
+
+		/**
+		 * The controller reference
+		 * @type {Object}
+		 */
+		var vm = this;
+		vm.loadPage = loadPage;
+        vm.customer = JSON.parse(JSON.stringify($rootScope.customer));
+        vm.saveCustomer = saveCustomer;
+        vm.goBack = goBack;
+
+
+				function goBack()
+				{
+					$state.go("customermanager");
+				}
+
+
+				function saveCustomer()
+				{
+						$ionicLoading.show({
+							template: 'Loading...'
+						})
+				console.debug(vm.customer);
+						Customer.update(vm.customer,function (data)
+						{
+							console.debug(data);
+								$rootScope.customer = data;
+								$ionicLoading.hide();
+								$state.go('customermanager');
+						}, function(error)
+						{
+							$ionicLoading.hide();
+						});
+				}
+
+
+	}
+})();
